78ab20feb4d28f94b52074f1ef44cde90c37233a
Gregory Pavier <gregory.pavier@ons.gov.uk>
2023-02-06 17:43:23 +0000

    Updated load.py to expand scope of the buildDataFrame class

--- src/my_package/load.py
+++ src/my_package/load.py
@@ -9,56 +9,56 @@ from csvcubed.cli.inspect.metadataprinter import (
 from csvcubed.utils.tableschema import CsvwRdfManager
 from csvcubed.utils.sparql_handler.data_cube_state import DataCubeState
 
-# Hardcoded json path
-# Should point to main metadata.json file provided by users in their CSVW
-csvw_metadata_json_path = Path(
-    "/Users/gregorypavier/project_dir/out/sweden-at-eurovision.csv-metadata.json"
-    # "/Users/gregorypavier/project_dir/out/ambulanc_response_times/ambulance-response-times-by-local-authority.csv-metadata.json"
-)
-
-# Directory that contains csv files
-csvw_directory = (
-    "/Users/gregorypavier/project_dir/out/"
-    # "/Users/gregorypavier/project_dir/out/ambulance_responses_times/"
-)
-
-csvw_rdf_manager = CsvwRdfManager(csvw_metadata_json_path)
-csvw_metadata_rdf_graph = csvw_rdf_manager.rdf_graph
-
-csvw_metadata_rdf_validator = MetadataValidator(
-    csvw_metadata_rdf_graph, csvw_metadata_json_path
-)
-
-csvw_type = csvw_metadata_rdf_validator.detect_csvw_type()
-
-data_cube = DataCubeState(
-    csvw_rdf_manager.rdf_graph, csvw_rdf_manager.csvw_metadata_file_path
-)
-
-metadata_printer = MetadataPrinter(
-    csvw_type=csvw_type,
-    csvw_metadata_rdf_graph=csvw_metadata_rdf_graph,
-    csvw_metadata_json_path=csvw_metadata_json_path,
-    data_cube_state=data_cube,
-    code_list_state=None,
-)
-
-metadata_printer.generate_general_results()
-
-dataset_uri = to_absolute_rdflib_file_path(
-    metadata_printer.result_catalog_metadata.dataset_uri, csvw_metadata_json_path
-)
-
-cube_identifiers = data_cube.get_cube_identifiers_for_data_set(
-    metadata_printer.result_catalog_metadata.dataset_uri
-)
-dsd_components = data_cube.get_dsd_qube_components_for_csv(cube_identifiers.csv_url)
-cube_shape = data_cube.get_shape_for_csv(cube_identifiers.csv_url)
-csv_path = csvw_metadata_json_path.parent / cube_identifiers.csv_url
-
 # Class that builds a pandas dataframe from csv file
 # Class also updates dataframe to use 'Lables' values instead of 'Notation' values
 class buildDataFrame:
+
+    # Hardcoded json path
+    # Should point to main metadata.json file provided by users in their CSVW
+    csvw_metadata_json_path = Path(
+        "/Users/gregorypavier/project_dir/out/sweden-at-eurovision.csv-metadata.json"
+        # "/Users/gregorypavier/project_dir/out/ambulanc_response_times/ambulance-response-times-by-local-authority.csv-metadata.json"
+    )
+
+    # Directory that contains csv files
+    csvw_directory = (
+        "/Users/gregorypavier/project_dir/out/"
+        # "/Users/gregorypavier/project_dir/out/ambulance_responses_times/"
+    )
+
+    csvw_rdf_manager = CsvwRdfManager(csvw_metadata_json_path)
+    csvw_metadata_rdf_graph = csvw_rdf_manager.rdf_graph
+
+    csvw_metadata_rdf_validator = MetadataValidator(
+        csvw_metadata_rdf_graph, csvw_metadata_json_path
+    )
+
+    csvw_type = csvw_metadata_rdf_validator.detect_csvw_type()
+
+    data_cube = DataCubeState(
+        csvw_rdf_manager.rdf_graph, csvw_rdf_manager.csvw_metadata_file_path
+    )
+
+    metadata_printer = MetadataPrinter(
+        csvw_type=csvw_type,
+        csvw_metadata_rdf_graph=csvw_metadata_rdf_graph,
+        csvw_metadata_json_path=csvw_metadata_json_path,
+        data_cube_state=data_cube,
+        code_list_state=None,
+    )
+
+    metadata_printer.generate_general_results()
+
+    dataset_uri = to_absolute_rdflib_file_path(
+        metadata_printer.result_catalog_metadata.dataset_uri, csvw_metadata_json_path
+    )
+
+    cube_identifiers = data_cube.get_cube_identifiers_for_data_set(
+        metadata_printer.result_catalog_metadata.dataset_uri
+    )
+    dsd_components = data_cube.get_dsd_qube_components_for_csv(cube_identifiers.csv_url)
+    cube_shape = data_cube.get_shape_for_csv(cube_identifiers.csv_url)
+    csv_path = csvw_metadata_json_path.parent / cube_identifiers.csv_url
 
     data = pd.read_csv(csv_path)
 
